43642c8e90ed4a26e848d86408f1b29c1aa2108f
aramel08 <78820331+aramel08@users.noreply.github.com>
2022-04-06 23:32:17 -0600

    Farmacia SAG Pantallas nuevas

---
+++ src/sag_farmacia/SAG_Farmacia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sag_farmacia;
+
+/**
+ *
+ * @author arame
+ */
+public class SAG_Farmacia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
